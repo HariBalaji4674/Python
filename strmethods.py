4664f044d6bf40659de6cdf45a498ac807b9219a
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2025-02-17 09:08:14 +0530

    Added HTML

--- strmethods.py
+++ strmethods.py
@@ -26,3 +26,34 @@ dog1.callBy()
 
 print(dog1.name,dog1.age)
 print(dog1.name,dog1.age,dog1.species)
+
+arr = [[1,2,3],[4,5,6]]
+length = len(arr[0])
+searchElement = 5
+print(length)
+for i in range(length-1):
+    for j in range(length):
+        if searchElement == arr[i][j]:
+            print(1)
+            break
+        else:
+            print(0)
+        # print(arr[i][j])
+# #User function Template for python3
+
+# class Solution:
+    
+#     #Function to search a given integer in a matrix.
+#     def searchMatrix(self,matrix, x): 
+#         # code here 
+#         for i in matrix:
+#             if(x in i):
+#                 return True
+                
+#         return False
+
+for i in arr:
+    if(searchElement in i):
+        print("true")
+        
+
